1af6c722ef89d5aa15923ff1c6bacd6081eb271c
DemiVlt <notch8537@gmail.com>
2024-10-11 16:51:45 -0500

    Introduction to Paging

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use small_os::{hlt_loop, println};
+use small_os::{hlt_loop, init, println};
+use x86_64::registers::control::Cr3;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}{}", "!", "!");
+    println!("Hello World{}", "!");
+
+    init();
+
+    let (lvl_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", lvl_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
 
-    small_os::init();
     println!("It didn't crash!");
-
     hlt_loop();
 }
 
